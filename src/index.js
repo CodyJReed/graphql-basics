15dbb8e4352782e2ef20cd7c474a809f50557eba
CodyJReed <cody2j2reed@gmail.com>
2021-02-06 23:01:37 -0800

    Added uuid and babel...rest-spread to simplify mutation parameters

--- src/index.js
+++ src/index.js
@@ -59,10 +59,30 @@ const comments = [
 // Type def (schema)
 const typeDefs = ` 
 type Mutation {
-	createUser(name: String!, email: String!, age: Int): User!
-	createPost(title: String!, body: String!, published: Boolean!, author: ID!): Post!
-	createComment(text: String!, author: ID!, post: ID!): Comment!
-},
+	createUser(data: CreateUserInput!): User!
+	createPost(data: CreatePostInput!): Post!
+	createComment(data: CreateCommentInput!): Comment!
+}
+
+input CreateUserInput {
+	name: String!
+	email: String!
+	age: Int
+}
+
+input CreatePostInput {
+	title: String!
+	body: String!,
+	published: Boolean!
+	author: ID!
+}
+
+input CreateCommentInput {
+	text: String!
+	author: ID!
+	post: ID!
+}
+
   type Query {
     users(query: String): [User!]!
     posts(query: String): [Post!]!
@@ -153,7 +173,7 @@ const resolvers = {
   // --- Create ---
   Mutation: {
     createUser(parent, args, ctx, info) {
-      const emailTaken = users.some((user) => user.email === args.email);
+      const emailTaken = users.some((user) => user.email === args.data.email);
 
       if (emailTaken) {
         throw new Error("Email taken.");
@@ -161,9 +181,7 @@ const resolvers = {
 
       const user = {
         id: uuidv4(),
-        name: args.name,
-        email: args.email,
-        age: args.age,
+        ...args.data,
       };
 
       users.push(user);
@@ -171,7 +189,7 @@ const resolvers = {
       return user;
     },
     createPost(parent, args, ctx, info) {
-      const userExists = users.some((user) => user.id === args.author);
+      const userExists = users.some((user) => user.id === args.data.author);
 
       if (!userExists) {
         throw new Error("User doesn't exist");
@@ -179,10 +197,7 @@ const resolvers = {
 
       const post = {
         id: uuidv4(),
-        title: args.title,
-        body: args.body,
-        published: args.published,
-        author: args.author,
+        ...args.data,
       };
 
       posts.push(post);
@@ -190,9 +205,9 @@ const resolvers = {
       return post;
     },
     createComment(parent, args, ctx, info) {
-      const userExists = users.some((user) => user.id === args.author);
+      const userExists = users.some((user) => user.id === args.data.author);
       const postExists = posts.some(
-        (post) => post.id === args.post && post.published
+        (post) => post.id === args.data.post && post.published
       );
 
       if (!userExists) {
@@ -205,9 +220,7 @@ const resolvers = {
 
       const comment = {
         id: uuidv4(),
-        text: args.text,
-        author: args.author,
-        post: args.post,
+        ...args.data,
       };
 
       comments.push(comment);
